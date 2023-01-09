d13e0ba34b2daa388fb992037fe095b26790416c
João Guilherme B. do Nascimento <joaoguilhermebn@hotmail.com>
2023-01-08 22:51:12 -0300

    second part frontend

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,60 +1,56 @@
-import { Container, Board, OrdersContainer } from './style';
+import { OrdersBoard } from '../OrdersBoard';
+import { Container} from './style';
 
+const orders: Order[] = [
+  {
+
+    '_id': '63b610bb4cfb40c1fa557fa4',
+    'table': '123',
+    'status': 'WAITING',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imagePath': '1672794386763-quatro-queijos.png',
+          'price': 40,
+
+        },
+        'quantity': 3,
+        '_id': '63b610bb4cfb40c1fa557fa5'
+      },
+      {
+        'product': {
+          'name': 'Coca Cola',
+          'imagePath': '1672795681796-coca-cola.png',
+          'price': 7,
+        },
+        'quantity': 2,
+        '_id': '63b610bb4cfb40c1fa557fa6'
+      }
+    ],
+  },
+
+
+];
 
 export function Orders() {
   return (
     <Container>
-      <Board>
-        <header>
-          <span>🕑</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-        <OrdersContainer>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
-      <Board>
-        <header>
-          <span>🕑</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-        <OrdersContainer>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
-      <Board>
-        <header>
-          <span>🕑</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-        <OrdersContainer>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
+      <OrdersBoard
+        icon = "🕑"
+        title = "Fila de Espera..."
+        orders = {orders}
+      />
+      <OrdersBoard
+        icon = "👩‍🍳"
+        title = "Em Produção."
+        orders = {[]}
+      />
+      <OrdersBoard
+        icon = "✅"
+        title = "Pronto!"
+        orders = {[]}
+      />
 
     </Container>
   );
---
+++ src/components/OrdersBoard/index.tsx
@@ -0,0 +1,36 @@
+import { Board, OrdersContainer } from './styles';
+
+
+interface OrderBoardsProps {
+  icon: string,
+  title: string,
+  orders: Order[];
+
+}
+
+export function OrdersBoard({icon, title}: OrderBoardsProps) {
+
+  return (
+
+    <Board>
+      <header>
+        <span>{icon}</span>
+        <strong>{title}</strong>
+        <span>(1)</span>
+      </header>
+      <OrdersContainer>
+        <button type='button'>
+          <strong>Mesa 2</strong>
+          <span>2 itens</span>
+        </button>
+        <button type='button'>
+          <strong>Mesa 2</strong>
+          <span>2 itens</span>
+        </button>
+      </OrdersContainer>
+    </Board>
+
+
+
+  );
+}
